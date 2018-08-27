f528b04cfcdf2eb24806326b987cef47e159206c
folkeza <42710741+folkeza@users.noreply.github.com>
2018-08-27 16:48:29 +0700

    Create bot_settings.php

---
+++ bot_settings.php
@@ -0,0 +1,6 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','1602524541');
+define('LINE_MESSAGE_CHANNEL_SECRET','01bc106e47cea5df418bb2314abcc1bb');
+define('LINE_MESSAGE_ACCESS_TOKEN','QaDbtuAUYq/i9S/HXHMDyEK8g9xIn4Anl2p29oJqNywNw/PUmHAHWSW8LiJQr7w4EQq1D9UFpAv7YmOcxY13dMdl2inK2zyF85Kf+qQ6+lfnJqGCpfUwPxePAM9HhfLnfYJsT0krbbaacwh9ellXnAdB04t89/1O/w1cDnyilFU=');
+?>
