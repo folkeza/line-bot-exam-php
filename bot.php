b6a70beebb8b4f7ded60e7d56fc0e7247b3a3b81
folkeza <42710741+folkeza@users.noreply.github.com>
2018-09-02 12:15:17 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -150,7 +150,7 @@ if(!is_null($events)){
             'Postback Text'  // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
         ),*/      
     );
-    $imageUrl = 'https://gdurl.com/OkqN';
+    $imageUrl = 'https://gdurl.com/XNmS';
     $replyData = new TemplateMessageBuilder('Button Template',
         new ButtonTemplateBuilder(
                 'DECORA CARE', // กำหนดหัวเรื่อง
